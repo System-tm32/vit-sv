51c76486c01cd028162187f61c65d9ee0040976d
System-tm32 <systemtm32@yandex.ru>
2023-04-27 22:26:30 +0300

    feat: add svg element

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
   import machine from './state/machine';
   import Header from './components/header/index.svelte';
   import MainContainer from './components/core/mainContainer.svelte';
+  import LogoSvg from './logoSvg.svelte';
 
   const { state, send } = useMachine(machine);
 </script>
 
 <MainContainer>
   <Header />
+  <svelte:component this={LogoSvg} />
 </MainContainer>
 
 <style>
